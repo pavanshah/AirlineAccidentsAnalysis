de85d09b0362a1c60d943e9142f87c9e50ad624f
Pavan Shah <pavan.shah@datos.io>
2017-10-21 14:16:57 -0700

    initial setup

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var http = require('http');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongo = require("./routes/mongo");
+var index = require('./routes/index');
+var users = require('./routes/users');
+var methodOverride = require('method-override');
+
+var mongoSessionConnectURL = "mongodb://pavan:pavan@ds229435.mlab.com:29435/airlineaccidents";
+
+var app = express();
+
+// view engine setup
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+//connect to the mongo collection session and then createServer
+mongo.connect(mongoSessionConnectURL, function(){
+	console.log('Connected to mongo at: ' + mongoSessionConnectURL);
+	http.createServer(app).listen(app.get('port'), function(){
+		console.log('Express server listening on port ' + app.get('port'));
+	});  
+});
+
+module.exports = app;
